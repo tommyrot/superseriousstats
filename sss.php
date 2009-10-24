226d5188138bd1ca72b48bab01c04c9a757c8408
tommyrot <jos@dutnie.nl>
2009-10-24 02:08:19 +0000

    if the MySQL database server is used and the mysqli extension isn't compiled into the php executable; exit with a warning

--- sss.php
+++ sss.php
@@ -40,6 +40,9 @@ if (!@include('settings.php'))
 if (!(count($argv) == 2 && $argv[1] == '-m') && !(count($argv) == 3 && ($argv[1] == '-i' || $argv[1] == '-o')) && !(count($argv) == 5 && $argv[1] == '-i' && $argv[3] == '-o'))
 	exit('usage: php '.basename(__FILE__).' {-i <log_file> [-o <html_file>] | -m | -o <html_file>}'."\n");
 
+if ($cfg['database_server'] == 'MySQL' && !extension_loaded('mysqli'))
+	exit('the mysqli extension isn\'t loaded'."\n");
+
 define('DB_HOST', $cfg['db_host']);
 define('DB_PORT', $cfg['db_port']);
 define('DB_USER', $cfg['db_user']);
@@ -81,7 +84,7 @@ function input($cfg, $log_file)
 		if (empty($yn) || $yn == 'y' || $yn == 'yes')
 			exit;
 	}
-	
+
 	define('DATE', date('Y-m-d', strtotime($tmp)));
 	define('DAY', strtolower(date('D', strtotime($tmp))));
 	$tmp = 'Parser_'.$cfg['logfile_format'];
