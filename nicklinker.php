b93fcf4896af72ce0d0ca0e77e67ffaa1fd28331
tommyrot <jos@dutnie.nl>
2009-12-24 06:27:10 +0000

    reworks

--- nicklinker.php
+++ nicklinker.php
@@ -16,17 +16,11 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/**
- * This script should be called from the commandline with PHP version 5.3.0 or up.
- *
- * usage: php nicklinker.php {-i <nicks_file> | -o <nicks_file>}
- *
- * Make sure to use the absolute path when specifying <nicks_file>.
- *
- * The options are:
- *	-i	import all users from <nicks_file> to the database
- *	-o	export all users from the database to <nicks_file>
- */
+$man = 'usage: php sss.php [-i <file>]'."\n"
+     . '       php sss.php [-o <file>]'."\n\n"
+     . 'the options are:'."\n"
+     . '	-i	import all users from <file> to the database'."\n"
+     . '	-o	export all users from the database to <file>'."\n";
 
 if (substr(phpversion(), 0, 3) != '5.3') {
 	exit('unsupported php version: '.phpversion()."\n");
@@ -45,7 +39,7 @@ if ($cfg['db_server'] == 'MySQL' && !extension_loaded('mysqli')) {
 }
 
 if (!(count($argv) == 3 && ($argv[1] == '-i' || $argv[1] == '-o'))) {
-	exit('usage: php '.basename(__FILE__).' {-i <nicks_file> | -o <nicks_file>}'."\n");
+	exit($man);
 }
 
 define('DB_HOST', $cfg['db_host']);
@@ -55,9 +49,7 @@ define('DB_PASS', $cfg['db_pass']);
 define('DB_NAME', $cfg['db_name']);
 
 if ($argv[1] == '-i') {
-	$fp = @fopen($argv[2], 'rb');
-
-	if ($fp) {
+	if (($fp = @fopen($argv[2], 'rb')) !== FALSE) {
 		$mysqli = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT) or exit('MySQL: '.mysqli_connect_error()."\n");
 		$query = @mysqli_query($mysqli, 'SELECT `UID`, `csNick` FROM `user_details`') or exit('MySQL: '.mysqli_error($mysqli)."\n");
 		$rows = mysqli_num_rows($query);
@@ -107,9 +99,7 @@ if ($argv[1] == '-i') {
 		exit('cannot open: '.$argv[2]."\n");
 	}
 } elseif ($argv[1] == '-o') {
-	$fp = @fopen($argv[2], 'wb');
-
-	if ($fp) {
+	if (($fp = @fopen($argv[2], 'wb')) !== FALSE) {
 		$mysqli = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT) or exit('MySQL: '.mysqli_connect_error()."\n");
 		$query = mysqli_query($mysqli, 'SELECT `RUID`, `status` FROM `user_status` WHERE `status` = 1 OR `status` = 3 ORDER BY `RUID` ASC');
 		$rows = mysqli_num_rows($query);
