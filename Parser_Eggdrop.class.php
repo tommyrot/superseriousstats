98fe1dcb67a0a1c82472864313394a867c1f0c5a
tommyrot <jos@dutnie.nl>
2010-01-24 17:06:32 +0000

    rewrite of the eggdrop parser to use regular expressions

--- Parser_Eggdrop.class.php
+++ Parser_Eggdrop.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (c) 2007-2009, Jos de Ruijter <jos@dutnie.nl>
+ * Copyright (c) 2007-2010, Jos de Ruijter <jos@dutnie.nl>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -17,12 +17,35 @@
  */
 
 /**
- * Parse instructions for Eggdrop logfile format.
+ * Parse instructions for the Eggdrop logfile format.
+ *
+ * +------------+-------------------------------------------------------+->
+ * | Line	| Format						| Notes
+ * +------------+-------------------------------------------------------+->
+ * | Normal	| <NICK> MSG						| Skip empty lines.
+ * | Action	| Action: NICK MSG					| Skip empty actions.
+ * | Slap	| Action: NICK slaps MSG				| Slaps may lack a (valid) target.
+ * | Nickchange	| Nick change: NICK -> NICK				|
+ * | Join	| NICK (HOST) joined CHAN.				|
+ * | Part	| NICK (HOST) left CHAN (MSG).				| Part message may be absent, or empty due to normalization.
+ * | Quit	| NICK (HOST) left irc: MSG				| Quit message may be empty due to normalization.
+ * | Mode	| CHAN: mode change '+o-v NICK NICK' by NICK!HOST	| Only check for combinations of ops (+o) and voices (+v). Host may be absent.
+ * | Topic	| Topic changed on CHAN by NICK!HOST: MSG		| Skip empty topics. Host may be absent.
+ * | Kick	| NICK kicked from CHAN by NICK: MSG			| Kick message may be empty due to normalization.
+ * | Repeat	| Last message repeated NUM time(s).			|
+ * +------------+-------------------------------------------------------+->
+ *
+ * Notes:
+ * - parseLog() normalizes all lines before passing them on to parseLine().
+ * - Given that nicks can't contain "<", ">" or ":" the order of the regular expressions below is irrelevant (current order aims for best performance).
+ * - The most common channel prefixes are "#&!+" and the most common nick prefixes are "~&@%+!*".
+ * - If there are multiple nicks we want to catch in our regular expression match we name the "performing" nick "nick1" and the "undergoing" nick "nick2".
+ * - Some converted mIRC logs do include "!" in "mode" and "topic" lines while there is no host.
  */
 final class Parser_Eggdrop extends Parser
 {
 	/**
-	 * Variable to prevent the parser from repeating a line endlessly.
+	 * Variables that shouldn't be tampered with.
 	 */
 	private $repeating = FALSE;
 
@@ -32,180 +55,103 @@ final class Parser_Eggdrop extends Parser
 	protected function parseLine($line)
 	{
 		/**
-		 * Only process lines beginning with a timestamp.
+		 * "Normal" lines.
 		 */
-		if (preg_match('/^\[([01][0-9]|2[0-3]):[0-5][0-9]\]/', $line)) {
-			$dateTime = $this->date.' '.substr($line, 1, 5);
-			$line = substr($line, 8);
-			$lineParts = explode(' ', $line);
+		if (preg_match('/^\[(?<time>\d{2}:\d{2})\] <(?<nick>\S+)> (?<line>.+)$/', $line, $matches)) {
+			$this->setNormal($this->date.' '.$matches['time'], $matches['nick'], $matches['line']);
 
-			/**
-			 * "Normal" lines. Format: "<NICK> MSG".
-			 */
-			if (preg_match('/^<.+>$/', $lineParts[0])) {
-				/**
-				 * Empty "normal" lines are silently ignored.
-				 */
-				if (isset($lineParts[1])) {
-					$csNick = trim($lineParts[0], '<>');
-					$line = implode(' ', array_slice($lineParts, 1));
-					$this->setNormal($dateTime, $csNick, $line);
-				}
+		/**
+		 * "Join" lines.
+		 */
+		} elseif (preg_match('/^\[(?<time>\d{2}:\d{2})\] (?<nick>\S+) \(~?(?<host>\S+)\) joined [#&!\+]\S+\.$/', $line, $matches)) {
+			$this->setJoin($this->date.' '.$matches['time'], $matches['nick'], $matches['host']);
 
-			/**
-			 * "Action" lines. Format: "Action: NICK MSG".
-			 * "Slap" lines. Format: "Action: NICK slaps MSG".
-			 */
-			} elseif ($lineParts[0] == 'Action:') {
-				/**
-				 * Empty "action" lines are silently ignored.
-				 */
-				if (isset($lineParts[2])) {
-					$csNick = $lineParts[1];
-					$line = implode(' ', array_slice($lineParts, 1));
+		/**
+		 * "Quit" lines.
+		 */
+		} elseif (preg_match('/^\[(?<time>\d{2}:\d{2})\] (?<nick>\S+) \(~?(?<host>\S+)\) left irc:( .+)?$/', $line, $matches)) {
+			$this->setQuit($this->date.' '.$matches['time'], $matches['nick'], $matches['host']);
 
-					/**
-					 * There doesn't have to be an "undergoing" nick for a slap to count.
-					 */
-					if (strtolower($lineParts[2]) == 'slaps') {
-						if (isset($lineParts[3])) {
-							$csNick_undergoing = $lineParts[3];
-						} else {
-							$csNick_undergoing = NULL;
-						}
+		/**
+		 * "Mode" lines.
+		 */
+		} elseif (preg_match('/^\[(?<time>\d{2}:\d{2})\] [#&!\+]\S+: mode change \'(?<modes>[-+][ov]+([-+][ov]+)?) (?<nicks>\S+( \S+)*)\' by (?<nick>\S+?)(!(~?(?<host>\S+))?)?$/', $line, $matches)) {
+			$nicks = explode(' ', $matches['nicks']);
+			$modeNum = 0;
 
-						$this->setSlap($dateTime, $csNick, $csNick_undergoing);
-					}
+			for ($i = 0; $i < strlen($matches['modes']); $i++) {
+				$mode = substr($matches['modes'], $i, 1);
 
-					$this->setAction($dateTime, $csNick, $line);
-				}
-
-			/**
-			 * "Nickchange" lines. Format: "Nick change: NICK -> NICK".
-			 */
-			} elseif ($lineParts[1] == 'change:') {
-				$csNick_performing = $lineParts[2];
-				$csNick_undergoing = $lineParts[4];
-				$this->setNickchange($dateTime, $csNick_performing, $csNick_undergoing);
-
-			/**
-			 * "Join" lines. Format: "NICK (HOST) joined CHAN.".
-			 */
-			} elseif ($lineParts[2] == 'joined') {
-				$csNick = $lineParts[0];
-				$csHost = trim($lineParts[1], '(~)');
-				$this->setJoin($dateTime, $csNick, $csHost);
-
-			/**
-			 * "Part" lines. Format: "NICK (HOST) left CHAN (MSG).".
-			 * "Quit" lines. Format: "NICK (HOST) left irc: MSG".
-			 */
-			} elseif ($lineParts[2] == 'left') {
-				$csNick = $lineParts[0];
-				$csHost = trim($lineParts[1], '(~)');
-
-				if ($lineParts[3] == 'irc:') {
-					$this->setQuit($dateTime, $csNick, $csHost);
+				if ($mode == '-' || $mode == '+') {
+					$modeSign = $mode;
 				} else {
-					$this->setPart($dateTime, $csNick, $csHost);
+					$this->setMode($this->date.' '.$matches['time'], $matches['nick'], $nicks[$modeNum], $modeSign.$mode, ($matches['host'] != '' ? $matches['host'] : NULL));
+					$modeNum++;
 				}
-
-			/**
-			 * "Mode" lines. Format: "CHAN: mode change '+o-v NICK NICK' by NICK!HOST".
-			 */
-			} elseif ($lineParts[1] == 'mode') {
-				$modes = ltrim($lineParts[3], '\'');
-
-				/**
-				 * Only process modes consisting of ops and voices.
-				 */
-				if (preg_match('/^[-+][ov]+([-+][ov]+)?$/', $modes)) {
-					$modesTotal = substr_count($modes, 'o') + substr_count($modes, 'v');
-
-					if (strpos($lineParts[5 + $modesTotal], '!') !== FALSE) {
-						$tmp = explode('!', $lineParts[5 + $modesTotal]);
-						$csNick_performing = $tmp[0];
-						$csHost = ltrim($tmp[1], '~');
-					} else {
-						$csNick_performing = $lineParts[5 + $modesTotal];
-						$csHost = NULL;
-					}
-
-					$modeNum = 0;
-
-					for ($i = 0; $i < strlen($modes); $i++) {
-						$mode = substr($modes, $i, 1);
-
-						if ($mode == '-' || $mode == '+') {
-							$modeSign = $mode;
-						} else {
-							$modeNum++;
-
-							if ($modeNum == $modesTotal) {
-								$csNick_undergoing = rtrim($lineParts[3 + $modeNum], '\'');
-							} else {
-								$csNick_undergoing = $lineParts[3 + $modeNum];
-							}
-
-							$this->setMode($dateTime, $csNick_performing, $csNick_undergoing, $modeSign.$mode, $csHost);
-						}
-					}
-				}
-
-			/**
-			 * "Topic" lines. Format: "Topic changed on CHAN by NICK!HOST: MSG".
-			 */
-			} elseif ($lineParts[1] == 'changed') {
-				$tmp = explode('!', $lineParts[5]);
-				$csNick = $tmp[0];
-				$csHost = trim($tmp[1], '~:');
-
-				/**
-				 * If the topic is empty we pass on NULL to setTopic().
-				 */
-				if (isset($lineParts[6])) {
-					$line = implode(' ', array_slice($lineParts, 6));
-				} else {
-					$line = NULL;
-				}
-
-				$this->setTopic($dateTime, $csNick, $csHost, $line);
-
-			/**
-			 * "Kick" lines. Format: "NICK kicked from CHAN by NICK: MSG".
-			 */
-			} elseif ($lineParts[1] == 'kicked') {
-				$csNick_performing = rtrim($lineParts[5], ':');
-				$csNick_undergoing = $lineParts[0];
-				$this->setKick($dateTime, $csNick_performing, $csNick_undergoing, $line);
-
-			/**
-			 * Eggdrops log repeated lines (case insensitive matches) in the format: "Last message repeated NUM time(s).".
-			 * We process the previous line NUM times.
-			 */
-			} elseif ($lineParts[1] == 'message') {
-				/**
-				 * Lock the repeating of lines to prevent a loop. Don't touch!!
-				 */
-				if (!$this->repeating) {
-					$this->repeating = TRUE;
-					$this->lineNum--;
-					$this->output('notice', 'parseLine(): repeating line '.$this->lineNum.': '.$lineParts[3].' '.(($lineParts[3] == 1) ? 'time' : 'times'));
-
-					for ($i = $lineParts[3]; $i > 0; $i--) {
-						$this->parseLine($this->prevLine);
-					}
-
-					$this->lineNum++;
-					$this->repeating = FALSE;
-				}
-
-			/**
-			 * Skip everything else.
-			 */
-			} else {
-				$this->output('notice', 'parseLine(): skipping line '.$this->lineNum.': \''.$line.'\'');
 			}
+
+		/**
+		 * "Action" and "slap" lines.
+		 */
+		} elseif (preg_match('/^\[(?<time>\d{2}:\d{2})\] Action: (?<line>(?<nick1>\S+) ((?<slap>[sS][lL][aA][pP][sS]( (?<nick2>\S+)( .+)?)?)|(.+)))$/', $line, $matches)) {
+			if ($matches['slap'] != '') {
+				$this->setSlap($this->date.' '.$matches['time'], $matches['nick1'], ($matches['nick2'] != '' ? $matches['nick2'] : NULL));
+			}
+
+			$this->setAction($this->date.' '.$matches['time'], $matches['nick1'], $matches['line']);
+
+		/**
+		 * "Nickchange" lines.
+		 */
+		} elseif (preg_match('/^\[(?<time>\d{2}:\d{2})\] Nick change: (?<nick1>\S+) -> (?<nick2>\S+)$/', $line, $matches)) {
+			$this->setNickchange($this->date.' '.$matches['time'], $matches['nick1'], $matches['nick2']);
+
+		/**
+		 * "Part" lines.
+		 */
+		} elseif (preg_match('/^\[(?<time>\d{2}:\d{2})\] (?<nick>\S+) \(~?(?<host>\S+)\) left [#&!\+]\S+( \(.*\))?\.$/', $line, $matches)) {
+			$this->setPart($this->date.' '.$matches['time'], $matches['nick'], ($matches['host'] != '' ? $matches['host'] : NULL));
+
+		/**
+		 * "Topic" lines.
+		 */
+		} elseif (preg_match('/^\[(?<time>\d{2}:\d{2})\] Topic changed on [#&!\+]\S+ by (?<nick>\S+?)(!(~?(?<host>\S+))?)?: (?<line>.+)$/', $line, $matches)) {
+			$this->setTopic($this->date.' '.$matches['time'], $matches['nick'], ($matches['host'] != '' ? $matches['host'] : NULL), $matches['line']);
+
+		/**
+		 * "Kick" lines.
+		 */
+		} elseif (preg_match('/^\[(?<time>\d{2}:\d{2})\] (?<line>(?<nick2>\S+) kicked from [#&!\+]\S+ by (?<nick1>\S+):( .+)?)$/', $line, $matches)) {
+			$this->setKick($this->date.' '.$matches['time'], $matches['nick1'], $matches['nick2'], $matches['line']);
+
+		/**
+		 * Eggdrop logs repeated lines (case insensitive matches) in the format: "Last message repeated NUM time(s).".
+		 * We process the previous line NUM times.
+		 */
+		} elseif (preg_match('/^\[(?<time>\d{2}:\d{2})\] Last message repeated (?<num>\d+) time\(s\)\.$/', $line, $matches)) {
+			/**
+			 * Prevent the parser from repeating a line endlessly.
+			 */
+			if ($this->repeating) {
+				return;
+			}
+
+			$this->repeating = TRUE;
+			$this->lineNum--;
+			$this->output('notice', 'parseLine(): repeating line '.$this->lineNum.': '.(($matches['num'] == 1) ? $matches['num'].' time' : $matches['num'].' times'));
+
+			for ($i = $matches['num']; $i > 0; $i--) {
+				$this->parseLine($this->prevLine);
+			}
+
+			$this->repeating = FALSE;
+			$this->lineNum++;
+
+		/**
+		 * Skip everything else.
+		 */
+		} else {
+			$this->output('notice', 'parseLine(): skipping line '.$this->lineNum.': \''.$line.'\'');
 		}
 	}
 }
