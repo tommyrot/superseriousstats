99ea376a66a9b2f20d4edff6c5b47602e76b2904
tommyrot <jos@dutnie.nl>
2011-02-03 16:45:15 +0100

    charset=utf-8 for the mainpage also

--- html.class.php
+++ html.class.php
@@ -188,7 +188,7 @@ final class html extends base
 		$this->output = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">'."\n\n"
 			      . '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">'."\n\n"
 			      . '<head>'."\n".'<title>'.htmlspecialchars($this->channel).', seriously.</title>'."\n"
-			      . '<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />'."\n"
+			      . '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />'."\n"
 			      . '<meta http-equiv="Content-Style-Type" content="text/css" />'."\n"
 			      . '<link rel="stylesheet" type="text/css" href="'.$this->stylesheet.'" />'."\n"
 			      . '<style type="text/css">'."\n"
