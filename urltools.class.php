530f657ba9145196bb6f358c566995a987dc2985
tommyrot <jos@dutnie.nl>
2011-04-07 20:21:05 +0200

    add functionality to the urltools class and clean up a bit

--- urltools.class.php
+++ urltools.class.php
@@ -17,116 +17,105 @@
  */
 
 /**
- * Various functions for urls following RFC 3986 and others.
+ * Various functions related to URL validation and presentation. This class is far from complete and only serves the basic needs of the superseriousstats program.
+ * It tries to follow RFC 3986. Take note of the following caveats if you want to use this class in other projects:
  *
  * Scheme:
- * - http:// and https:// only
+ * - http:// and https:// only.
  *
  * Authority:
- * - Following preferred syntax, RFC 1034 section 3.5 and RFC 1123 section 2.1
- * - There is no overall length check, only labels are checked on length (max 63 characters)
- * - No user part
+ * - Following preferred syntax, RFC 1034 section 3.5 and RFC 1123 section 2.1.
+ * - There is no overall length check, only labels are checked on length (max 63 characters).
+ * - No user part.
  *
  * IP:
- * - IPv4 only
+ * - IPv4 only.
  * - 0.0.0.0 to 255.255.255.255
- * - No leading zeros
+ * - No leading zeros.
  *
  * Port:
  * - 0 to 65535
- * - No leading zeros
+ * - No leading zeros.
  *
  * TLD:
- * - http://data.iana.org/TLD/tlds-alpha-by-domain.txt # Version 2010080701, Last Updated Sun Aug  8 14:07:01 2010 UTC
- * - No ASCII variants of internationalized country codes (xn--)
+ * - http://data.iana.org/TLD/tlds-alpha-by-domain.txt (2011040700).
+ * - No ASCII variants of internationalized country codes (xn--).
  *
  * Other:
- * - Square brackets must be percent encoded
+ * - Square brackets must be percent encoded.
  */
 final class urltools
 {
-	/**
-	 * Character groups as described in RFC 3986.
-	 */
-	private $gen_delims = '';
-	private $pchar = '';
-	private $pct_encoded = '';
-	private $reserved = '';
-	private $sub_delims = '';
-	private $unreserved = '';
-
-	/**
-	 * Elements of the regular expression.
-	 */
-	private $ipv4address = '';
 	private $authority = '';
-	private $domain = '';
+	private $domain = '[a-z0-9]([a-z0-9-]{0,61}?[a-z0-9]|[a-z0-9]{0,62})?(\.[a-z0-9]([a-z0-9-]{0,61}?[a-z0-9]|[a-z0-9]{0,62})?)*';
 	private $fqdn = '';
 	private $fragment = '';
+	private $gen_delims = '[]:\/?#[@]';
+	private $ipv4address = '(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])(\.(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])){3}';
 	private $path = '';
-	private $port = '';
+	private $pchar = '';
+	private $pct_encoded = '%[0-9a-f]{2}';
+	private $port = '(6553[0-5]|(655[0-2]|(65[0-4]|(6[0-4]|[1-5][0-9]|[1-9])[0-9]|[1-9])[0-9]|[1-9])?[0-9])';
 	private $query = '';
-	private $scheme = '';
-	private $tld = '';
+	private $reserved = '';
+	private $scheme = '(https?:\/\/)?';
+	private $sub_delims = '[!$&\'()*+,;=]';
+	private $tld = '\.(ac|ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|asia|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cat|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|info|int|io|iq|ir|is|it|je|jm|jo|jobs|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mobi|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tel|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|travel|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw)\.?';
+	private $unreserved = '[a-z0-9_.~-]';
 
-	/**
-	 * The regular expression itself.
-	 */
-	private $regexp = '';
-
-	/**
-	 * Constructor.
-	 */
 	public function __construct()
 	{
-		/**
-		 * Character groups.
-		 */
-		$this->unreserved = '[a-z0-9_.~-]';
-		$this->pct_encoded = '%[0-9a-f]{2}';
-		$this->gen_delims = '[]:\/?#[@]';
-		$this->sub_delims = '[!$&\'()*+,;=]';
 		$this->reserved = '('.$this->gen_delims.'|'.$this->sub_delims.')';
 		$this->pchar = '('.$this->unreserved.'|'.$this->pct_encoded.'|'.$this->sub_delims.'|[:@])';
-
-		/**
-		 * Elements.
-		 */
-		$this->scheme = 'https?:\/\/';
-		$this->ipv4address = '(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])(\.(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])){3}';
-		$this->port = '(6553[0-5]|(655[0-2]|(65[0-4]|(6[0-4]|[1-5][0-9]|[1-9])[0-9]|[1-9])[0-9]|[1-9])?[0-9])';
-		$this->domain = '[a-z0-9]([a-z0-9-]{0,61}?[a-z0-9]|[a-z0-9]{0,62})?(\.[a-z0-9]([a-z0-9-]{0,61}?[a-z0-9]|[a-z0-9]{0,62})?)*';
-		$this->tld = '\.(ac|ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|asia|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cat|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|info|int|io|iq|ir|is|it|je|jm|jo|jobs|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mobi|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tel|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|travel|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|za|zm|zw)\.?';
 		$this->fqdn = $this->domain.$this->tld;
 		$this->authority = '('.$this->ipv4address.'|'.$this->fqdn.')(:'.$this->port.')?';
+		$this->fragment = '(#('.$this->pchar.'|[\/?])*)?';
 		$this->path = '(\/\/?('.$this->pchar.'+\/?)*)?';
 		$this->query = '(\?('.$this->pchar.'|[\/?])*)?';
-		$this->fragment = '(#('.$this->pchar.'|[\/?])*)?';
-
-		/**
-		 * Build regular expression. Case insensitive.
-		 */
-		$this->regexp = '/^'.$this->scheme.$this->authority.$this->path.$this->query.$this->fragment.'$/i';
 	}
 
 	/**
-	 * Normalize a url.
+	 * Return the FQDN.
+	 */
+	public function get_fqdn($csurl) {
+		if (preg_match('/^'.$this->scheme.'(?<fqdn>'.$this->fqdn.')'.$this->path.$this->query.$this->fragment.'$/i', $csurl, $matches)) {
+			return $matches['fqdn'];
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Return the TLD.
+	 */
+	public function get_tld($csurl) {
+		if (preg_match('/^'.$this->scheme.$this->domain.'(?<tld>'.$this->tld.')'.$this->path.$this->query.$this->fragment.'$/i', $csurl, $matches)) {
+			return $matches['tld'];
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Normalize a URL.
 	 */
 	public function normalize_url($csurl)
 	{
 		/**
-		 * Convert scheme and authority to lower case.
+		 * 1. Convert scheme and authority to lower case.
+		 * 2. Put "http://" scheme in front of a URL beginning with just "www.".
 		 */
-		$csurl = preg_replace('/^'.$this->scheme.$this->authority.'/ei', 'strtolower(\'$0\')', $csurl);
+
+		$csurl = preg_replace(array('/^'.$this->scheme.$this->authority.'/ei', '/^www\./'), array('strtolower(\'$0\')', 'http://$0'), $csurl);
 		return $csurl;
 	}
 
 	/**
-	 * Validate a url.
+	 * Validate a URL.
 	 */
 	public function validate_url($csurl)
 	{
-		if (preg_match($this->regexp, $csurl)) {
+		if (preg_match('/^'.$this->scheme.$this->authority.$this->path.$this->query.$this->fragment.'$/i', $csurl)) {
 			return true;
 		} else {
 			return false;
