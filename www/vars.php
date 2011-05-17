ae00dfa6b0b543ff4aaeac4f2f0e0f5218da62e0
tommyrot <jos@dutnie.nl>
2011-05-17 17:38:51 +0200

    update vars.php

--- www/vars.php
+++ www/vars.php
@@ -1,35 +1,37 @@
 <?php
 
-/**
- * Global settings are the default for ALL channels.
- */
+// These settings only apply to user.php and history.php.
+
+// Global settings are the default for ALL channels.
 $settings['__global'] = array(
-'bar_afternoon' => 'y.png',
-'bar_evening' => 'r.png',
-'bar_morning' => 'g.png',
-'bar_night' => 'b.png',
-'channel' => '',
-'db_host' => '127.0.0.1',
-'db_name' => 'sss',
-'db_pass' => '',
-'db_port' => 3306,
-'db_user' => '',
-'mainpage' => './',
-'rows_people_month' => 30,
-'rows_people_timeofday' => 10,
-'rows_people_year' => 30,
-'stylesheet' => 'sss.css',
-'timezone' => 'UTC',
-'userstats' => false
+	'bar_afternoon' => 'y.png',
+	'bar_evening' => 'r.png',
+	'bar_morning' => 'g.png',
+	'bar_night' => 'b.png',
+	'channel' => '',
+	'db_host' => '127.0.0.1',
+	'db_name' => 'sss',
+	'db_pass' => '',
+	'db_port' => 3306,
+	'db_user' => '',
+	'debug' => false,
+	'mainpage' => './',
+	'rows_people_month' => 30,
+	'rows_people_timeofday' => 10,
+	'rows_people_year' => 30,
+	'stylesheet' => 'sss.css',
+	'timezone' => 'UTC',
+	'userstats' => false
 );
 
-/**
- * Each channel can have their own specific settings overriding global ones.
- * _'example' is the channel ID here_
- */
-$settings['example'] = array(
-'channel' => '#example',
-'db_name' => 'sss-example'
+// Each channel can have their own specific settings overriding the global ones.
+// Create a new array with the appropriate channel ID for each channel and put
+// the settings in it that differ from the global values.
+$settings['MyChannelID'] = array(
+	'channel' => '#MyChannel',
+	'db_name' => 'MyDatabase',
+	'mainpage' => './mychannel.html',
+	'userstats' => true
 );
 
 ?>
