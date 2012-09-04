18b0ec2799c34c388d8685eda2fde4c82844dd0b
tommyrot <jos@dutnie.nl>
2012-09-04 22:30:20 +0200

    badger badger badger badger badger badger badger badger badger badger badger badger badger badger badger badger badger badger

--- maintenance.class.php
+++ maintenance.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (c) 2007-2011, Jos de Ruijter <jos@dutnie.nl>
+ * Copyright (c) 2007-2012, Jos de Ruijter <jos@dutnie.nl>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
