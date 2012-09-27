b487039bbfb32579e5d69f5f85177b710b1219cf
tommyrot <jos@dutnie.nl>
2012-09-27 02:27:23 +0200

    words table can be empty, check

--- html.php
+++ html.php
@@ -700,19 +700,22 @@ final class html extends base
 			 * Display the top 9 word tables ordered by totals.
 			 */
 			$query = @mysqli_query($this->mysqli, 'select `length`, count(*) as `total` from `words` group by `length` order by `total` desc, `length` desc limit 9') or $this->output('critical', 'mysqli: '.mysqli_error($this->mysqli));
+			$rows = mysqli_num_rows($query);
 
-			while ($result = mysqli_fetch_object($query)) {
-				$t = new table('Words of '.$result->length.' Characters');
-				$t->set_value('key1', 'Times Used');
-				$t->set_value('key2', 'Word');
-				$t->set_value('minrows', $this->minrows);
-				$t->set_value('query_main', 'select `total` as `v1`, `word` as `v2` from `words` where `length` = '.$result->length.' order by `v1` desc, `v2` asc limit 5');
-				$t->set_value('total', (int) $result->total);
-				$output .= $t->make_table($this->mysqli);
-			}
+			if (!empty($rows)) {
+				while ($result = mysqli_fetch_object($query)) {
+					$t = new table('Words of '.$result->length.' Characters');
+					$t->set_value('key1', 'Times Used');
+					$t->set_value('key2', 'Word');
+					$t->set_value('minrows', $this->minrows);
+					$t->set_value('query_main', 'select `total` as `v1`, `word` as `v2` from `words` where `length` = '.$result->length.' order by `v1` desc, `v2` asc limit 5');
+					$t->set_value('total', (int) $result->total);
+					$output .= $t->make_table($this->mysqli);
+				}
 
-			if (!empty($output)) {
-				$this->output .= "\n".'<div class="head">Words</div>'."\n".$output;
+				if (!empty($output)) {
+					$this->output .= "\n".'<div class="head">Words</div>'."\n".$output;
+				}
 			}
 		}
 
