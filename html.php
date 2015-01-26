bce00a5aab8deefaa6c2df9f325cbfc2dc66a1d4
tommyrot <jos@dutnie.nl>
2015-01-26 04:33:36 +0100

    bump

--- html.php
+++ html.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (c) 2007-2014, Jos de Ruijter <jos@dutnie.nl>
+ * Copyright (c) 2007-2015, Jos de Ruijter <jos@dutnie.nl>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
