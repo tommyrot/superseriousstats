1fdd3383706414af44b4d1f8794283ac90aca71f
tommyrot <jos@dutnie.nl>
2015-02-07 14:14:29 +0100

    convert urltools to static

--- urltools.php
+++ urltools.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (c) 2007-2014, Jos de Ruijter <jos@dutnie.nl>
+ * Copyright (c) 2007-2015, Jos de Ruijter <jos@dutnie.nl>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -28,52 +28,61 @@
  * - The root domain is excluded from the FQDN (not from the other elements).
  * - Square brackets must be percent encoded.
  */
-final class urltools
+class urltools
 {
-	private $authority = '';
-	private $domain = '(?<domain>[a-z0-9]([a-z0-9-]{0,61}?[a-z0-9]|[a-z0-9]{0,62})?(\.[a-z0-9]([a-z0-9-]{0,61}?[a-z0-9]|[a-z0-9]{0,62})?)*)';
-	private $fqdn = '';
-	private $fragment = '';
-	private $gen_delims = '[]:\/?#[@]';
-	private $ipv4address = '(?<ipv4address>(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])(\.(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])){3})';
-	private $path = '';
-	private $pchar = '';
-	private $pct_encoded = '%[0-9a-f]{2}';
-	private $port = '(?<port>(6553[0-5]|(655[0-2]|(65[0-4]|(6[0-4]|[1-5][0-9]|[1-9])[0-9]|[1-9])[0-9]|[1-9])?[0-9]))';
-	private $query = '';
-	private $reserved = '';
-	private $scheme = '(?<scheme>https?:\/\/)';
-	private $sub_delims = '[!$&\'()*+,;=]';
-	private $tld = '(?<tld>\.(ac|ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|asia|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cat|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cu|cv|cw|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|info|int|io|iq|ir|is|it|je|jm|jo|jobs|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mobi|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|post|pr|pro|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sx|sy|sz|tc|td|tel|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|travel|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|xxx|ye|yt|za|zm|zw))';
-	private $unreserved = '[a-z0-9_.~-]';
+	private static $authority = '';
+	private static $domain = '(?<domain>[a-z0-9]([a-z0-9-]{0,61}?[a-z0-9]|[a-z0-9]{0,62})?(\.[a-z0-9]([a-z0-9-]{0,61}?[a-z0-9]|[a-z0-9]{0,62})?)*)';
+	private static $fqdn = '';
+	private static $fragment = '';
+	private static $gen_delims = '[]:\/?#[@]';
+	private static $ipv4address = '(?<ipv4address>(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])(\.(25[0-5]|(2[0-4]|1[0-9]|[1-9])?[0-9])){3})';
+	private static $path = '';
+	private static $pchar = '';
+	private static $pct_encoded = '%[0-9a-f]{2}';
+	private static $port = '(?<port>(6553[0-5]|(655[0-2]|(65[0-4]|(6[0-4]|[1-5][0-9]|[1-9])[0-9]|[1-9])[0-9]|[1-9])?[0-9]))';
+	private static $query = '';
+	private static $reserved = '';
+	private static $scheme = '(?<scheme>https?:\/\/)';
+	private static $sub_delims = '[!$&\'()*+,;=]';
+	private static $tld = '(?<tld>\.(ac|ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|asia|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cat|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cu|cv|cw|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|info|int|io|iq|ir|is|it|je|jm|jo|jobs|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mobi|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|post|pr|pro|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sx|sy|sz|tc|td|tel|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|travel|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|xxx|ye|yt|za|zm|zw))';
+	private static $unreserved = '[a-z0-9_.~-]';
 
-	public function __construct()
+	private function __construct()
 	{
-		$this->fqdn = '(?<fqdn>'.$this->domain.$this->tld.')\.?';
-		$this->authority = '(?<authority>('.$this->ipv4address.'|'.$this->fqdn.')(:'.$this->port.')?)';
-		$this->pchar = '('.$this->unreserved.'|'.$this->pct_encoded.'|'.$this->sub_delims.'|[:@])';
-		$this->fragment = '(?<fragment>(#('.$this->pchar.'|[\/?])*)?)';
-		$this->path = '(?<path>(\/\/?('.$this->pchar.'+\/?)*)?)';
-		$this->query = '(?<query>(\?('.$this->pchar.'|[\/?])*)?)';
-		$this->reserved = '('.$this->gen_delims.'|'.$this->sub_delims.')';
+		/**
+		 * This is a static class and should not be instantiated.
+		 */
 	}
 
 	/**
 	 * Normalize and validate a URL and return an array with its elements.
 	 */
-	public function get_elements($url)
+	public static function get_elements($url)
 	{
+		/**
+		 * Assemble parts of the regular expression if not already done so.
+		 */
+		if (empty(self::$fqdn)) {
+			self::$fqdn = '(?<fqdn>'.self::$domain.self::$tld.')\.?';
+			self::$authority = '(?<authority>('.self::$ipv4address.'|'.self::$fqdn.')(:'.self::$port.')?)';
+			self::$pchar = '('.self::$unreserved.'|'.self::$pct_encoded.'|'.self::$sub_delims.'|[:@])';
+			self::$fragment = '(?<fragment>(#('.self::$pchar.'|[\/?])*)?)';
+			self::$path = '(?<path>(\/\/?('.self::$pchar.'+\/?)*)?)';
+			self::$query = '(?<query>(\?('.self::$pchar.'|[\/?])*)?)';
+			self::$reserved = '('.self::$gen_delims.'|'.self::$sub_delims.')';
+		}
+
 		/**
 		 * Convert scheme and authority to lower case.
 		 */
-		$url = preg_replace_callback('/^'.$this->scheme.'?'.$this->authority.'/i', function ($matches) {
+		$url = preg_replace_callback('/^'.self::$scheme.'?'.self::$authority.'/i', function ($matches) {
 			return strtolower($matches[0]);
 		}, $url);
 
 		/**
 		 * Validate and further process the URL.
 		 */
-		if (preg_match('/^(?<url>'.$this->scheme.'?'.$this->authority.$this->path.$this->query.$this->fragment.')$/i', $url, $matches)) {
+		if (preg_match('/^(?<url>'.self::$scheme.'?'.self::$authority.self::$path.self::$query.self::$fragment.')$/i', $url, $matches)) {
 			/**
 			 * The maximum allowed length of the FQDN (root domain excluded) is 254 characters.
 			 */
