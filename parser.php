514a3049eb58f9b3d7bc29ec8101f3307a68d8f0
tommyrot <jos@dutnie.nl>
2020-05-04 17:43:42 +0200

    simplify away intdiv

--- parser.php
+++ parser.php
@@ -543,7 +543,7 @@ class parser
 			if (mb_strlen(preg_replace('/\P{Lu}+/u', '', $line), 'UTF-8') * 2 > $line_length) {
 				$this->nick_objs[$nick]->add_int('uppercased', 1);
 
-				if (!$skip_quote && ($this->nick_objs[$nick]->get_string('ex_uppercased') === '' || rand(1, 100) <= (intdiv($line_length, 30) === 0 ? 25 : 50))) {
+				if (!$skip_quote && ($this->nick_objs[$nick]->get_string('ex_uppercased') === '' || rand(1, 100) <= ($line_length < 30 ? 25 : 50))) {
 					$this->nick_objs[$nick]->set_string('ex_uppercased', $line);
 					$skip_quote = true;
 				}
@@ -553,20 +553,20 @@ class parser
 		if (preg_match('/!$/', $line)) {
 			$this->nick_objs[$nick]->add_int('exclamations', 1);
 
-			if (!$skip_quote && ($this->nick_objs[$nick]->get_string('ex_exclamations') === '' || rand(1, 100) <= (intdiv($line_length, 30) === 0 ? 25 : 50))) {
+			if (!$skip_quote && ($this->nick_objs[$nick]->get_string('ex_exclamations') === '' || rand(1, 100) <= ($line_length < 30 ? 25 : 50))) {
 				$this->nick_objs[$nick]->set_string('ex_exclamations', $line);
 				$skip_quote = true;
 			}
 		} elseif (preg_match('/\?$/', $line)) {
 			$this->nick_objs[$nick]->add_int('questions', 1);
 
-			if (!$skip_quote && ($this->nick_objs[$nick]->get_string('ex_questions') === '' || rand(1, 100) <= (intdiv($line_length, 30) === 0 ? 25 : 50))) {
+			if (!$skip_quote && ($this->nick_objs[$nick]->get_string('ex_questions') === '' || rand(1, 100) <= ($line_length < 30 ? 25 : 50))) {
 				$this->nick_objs[$nick]->set_string('ex_questions', $line);
 				$skip_quote = true;
 			}
 		}
 
-		if (!$skip_quote && ($this->nick_objs[$nick]->get_string('quote') === '' || rand(1, 100) <= (intdiv($line_length, 30) === 0 ? 5 : 25))) {
+		if (!$skip_quote && ($this->nick_objs[$nick]->get_string('quote') === '' || rand(1, 100) <= ($line_length < 30 ? 5 : 25))) {
 			$this->nick_objs[$nick]->set_string('quote', $line);
 		}
 	}
