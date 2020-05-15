d5fb7d71c95c59be82e94dc8466d3141e42e665e
tommyrot <jos@dutnie.nl>
2020-05-15 02:06:57 +0200

    optimize flow

--- parser.php
+++ parser.php
@@ -602,11 +602,13 @@ class parser
 		}
 
 		/**
-		 * Store channel totals.
+		 * Store channel totals and streak history.
 		 */
 		if ($this->l_total !== 0) {
 			$queryparts = $this->get_queryparts(['l_00', 'l_01', 'l_02', 'l_03', 'l_04', 'l_05', 'l_06', 'l_07', 'l_08', 'l_09', 'l_10', 'l_11', 'l_12', 'l_13', 'l_14', 'l_15', 'l_16', 'l_17', 'l_18', 'l_19', 'l_20', 'l_21', 'l_22', 'l_23', 'l_night', 'l_morning', 'l_afternoon', 'l_evening', 'l_total']);
 			db::query_exec('INSERT INTO channel_activity (date, '.$queryparts['insert_columns'].') VALUES (\''.$this->date.'\', '.$queryparts['insert_values'].') ON CONFLICT (date) DO UPDATE SET '.$queryparts['update_assignments']);
+			db::query_exec('DELETE FROM streak_history');
+			db::query_exec('INSERT INTO streak_history (nick_prev, streak) VALUES (\''.$this->nick_prev.'\', '.$this->streak.')');
 		}
 
 		/**
@@ -635,14 +637,6 @@ class parser
 		 */
 		foreach ($this->word_objs as $word) {
 			$word->store_data();
-		}
-
-		/**
-		 * Store streak history.
-		 */
-		if ($this->l_total !== 0) {
-			db::query_exec('DELETE FROM streak_history');
-			db::query_exec('INSERT INTO streak_history (nick_prev, streak) VALUES (\''.$this->nick_prev.'\', '.$this->streak.')');
 		}
 
 		return true;
