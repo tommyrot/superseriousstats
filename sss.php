1988f534b82a5718c78bb22ceda393215ddcb818
tommyrot <jos@dutnie.nl>
2020-05-24 20:43:19 +0200

    parentheses not needed, _once makes no sense

--- sss.php
+++ sss.php
@@ -24,9 +24,9 @@ foreach (['sqlite3', 'mbstring', 'zlib'] as $module) {
  */
 spl_autoload_register(function (string $class): void {
 	if (strpos($class, 'parser_') === 0) { #str_starts_with() PHP8
-		require_once(__DIR__.'/parsers/'.substr($class, 7).'.php');
+		require __DIR__.'/parsers/'.substr($class, 7).'.php';
 	} else {
-		require_once(__DIR__.'/'.$class.'.php');
+		require __DIR__.'/'.$class.'.php';
 	}
 });
 
