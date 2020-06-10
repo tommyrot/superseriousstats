773a82846c11f8ce785bc28d530c875cf205cbfb
tommyrot <jos@dutnie.nl>
2020-06-10 22:54:06 +0200

    cleanup

--- parsers/nodelog.php
+++ parsers/nodelog.php
@@ -8,20 +8,15 @@ class parser_nodelog extends parser
 {
 	protected function parse_line(string $line): void
 	{
-		// "Normal" lines.
-		if (preg_match('/^\[(?<time>\d{2}:\d{2}(:\d{2})?)\] (?<nick>\S+): (?<line>.+)$/', $line, $matches)) {
+		$timestamp = '\[(?<time>\d{2}:\d{2}(:\d{2})?)\] ';
+
+		if (preg_match('/^'.$timestamp.'(?<nick>\S+): (?<line>.+)$/', $line, $matches)) {
 			$this->set_normal($matches['time'], $matches['nick'], $matches['line']);
-
-		// "Join" lines.
-		} elseif (preg_match('/^\[(?<time>\d{2}:\d{2}(:\d{2})?)\] (?<nick>\S+) has joined the channel$/', $line, $matches)) {
+		} elseif (preg_match('/^'.$timestamp.'(?<nick>\S+) has joined the channel$/', $line, $matches)) {
 			$this->set_join($matches['time'], $matches['nick']);
-
-		// "Part" lines.
-		} elseif (preg_match('/^\[(?<time>\d{2}:\d{2}(:\d{2})?)\] (?<nick>\S+) has left the channel$/', $line, $matches)) {
+		} elseif (preg_match('/^'.$timestamp.'(?<nick>\S+) has left the channel$/', $line, $matches)) {
 			$this->set_part($matches['time'], $matches['nick']);
-
-		// Skip everything else.
-		} elseif ($line !== '') {
+		} else {
 			out::put('debug', 'skipping line '.$this->linenum.': \''.$line.'\'');
 		}
 	}
