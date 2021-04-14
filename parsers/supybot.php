548a6ac1c03cdda5bff9500a1ab0c69de8a21521
tommyrot <jos@dutnie.nl>
2021-04-14 20:35:44 +0200

    cleanup and simplify regexp when possible; prevent empty kick message

--- parsers/supybot.php
+++ parsers/supybot.php
@@ -12,11 +12,11 @@ class parser_supybot extends parser
 
 		if (preg_match('/^'.$timestamp.'<(?<nick>\S+)> (?<line>.+)$/', $line, $matches)) {
 			$this->set_normal($matches['time'], $matches['nick'], $matches['line']);
-		} elseif (preg_match('/^'.$timestamp.'\*\*\* (?<nick>\S+) has joined [#&!+]\S+$/', $line, $matches)) {
+		} elseif (preg_match('/^'.$timestamp.'\*\*\* (?<nick>\S+) has joined/', $line, $matches)) {
 			$this->set_join($matches['time'], $matches['nick']);
-		} elseif (preg_match('/^'.$timestamp.'\*\*\* (?<nick>\S+) has quit IRC$/', $line, $matches)) {
+		} elseif (preg_match('/^'.$timestamp.'\*\*\* (?<nick>\S+) has quit IRC/', $line, $matches)) {
 			$this->set_quit($matches['time'], $matches['nick']);
-		} elseif (preg_match('/^'.$timestamp.'\* (?<line>(?<nick_performing>\S+) ((?<slap>slaps( (?<nick_undergoing>\S+)( .+)?))|(.+)))$/i', $line, $matches, PREG_UNMATCHED_AS_NULL)) {
+		} elseif (preg_match('/^'.$timestamp.'\* (?<line>(?<nick_performing>\S+) ((?<slap>slaps (?<nick_undergoing>\S+).*)|.+))$/i', $line, $matches, PREG_UNMATCHED_AS_NULL)) {
 			if (!is_null($matches['slap'])) {
 				$this->set_slap($matches['time'], $matches['nick_performing'], $matches['nick_undergoing']);
 			}
@@ -38,12 +38,12 @@ class parser_supybot extends parser
 					++$mode_num;
 				}
 			}
-		} elseif (preg_match('/^'.$timestamp.'\*\*\* (?<nick>\S+) has left [#&!+]\S+$/', $line, $matches)) {
+		} elseif (preg_match('/^'.$timestamp.'\*\*\* (?<nick>\S+) has left/', $line, $matches)) {
 			$this->set_part($matches['time'], $matches['nick']);
 		} elseif (preg_match('/^'.$timestamp.'\*\*\* (?<nick>\S+) changes topic to "(?<line>.+)"$/', $line, $matches) && $matches['line'] !== ' ') {
 			$this->set_topic($matches['time'], $matches['nick'], $matches['line']);
-		} elseif (preg_match('/^'.$timestamp.'\*\*\* (?<line>(?<nick_undergoing>\S+) was kicked by (?<nick_performing>\S+) \(.*\))$/', $line, $matches)) {
-			$this->set_kick($matches['time'], $matches['nick_performing'], $matches['nick_undergoing'], $matches['line']);
+		} elseif (preg_match('/^'.$timestamp.'\*\*\* (?<line>(?<nick_undergoing>\S+) was kicked by (?<nick_performing>\S+) )(?<reason>\(.*\))$/', $line, $matches)) {
+			$this->set_kick($matches['time'], $matches['nick_performing'], $matches['nick_undergoing'], $matches['line'].($matches['reason'] === '()' ? '('.$matches['nick_undergoing'].')' : $matches['reason']));
 		} else {
 			out::put('debug', 'skipping line '.$this->linenum.': \''.$line.'\'');
 		}
