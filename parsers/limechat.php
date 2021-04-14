1ebe146bb252dce7e4ffbd4ed633ee90eb9bbef4
tommyrot <jos@dutnie.nl>
2021-04-14 16:52:29 +0200

    cleanup and simplify regexp when possible; prevent empty kick message

--- parsers/limechat.php
+++ parsers/limechat.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 
 /**
- * Copyright (c) 2012-2020, Jos de Ruijter <jos@dutnie.nl>
+ * Copyright (c) 2012-2021, Jos de Ruijter <jos@dutnie.nl>
  */
 
 class parser_limechat extends parser
@@ -12,9 +12,9 @@ class parser_limechat extends parser
 
 		if (preg_match('/^'.$timestamp.'(?<nick>\S+): (?<line>.+)$/', $line, $matches)) {
 			$this->set_normal($matches['time'], $matches['nick'], $matches['line']);
-		} elseif (preg_match('/^'.$timestamp.'(?<nick>\S+) has joined \(\S+\)$/', $line, $matches)) {
+		} elseif (preg_match('/^'.$timestamp.'(?<nick>\S+) has joined/', $line, $matches)) {
 			$this->set_join($matches['time'], $matches['nick']);
-		} elseif (preg_match('/^'.$timestamp.'(?<nick>\S+) has left IRC \(.*\)$/', $line, $matches)) {
+		} elseif (preg_match('/^'.$timestamp.'(?<nick>\S+) has left IRC/', $line, $matches)) {
 			$this->set_quit($matches['time'], $matches['nick']);
 		} elseif (preg_match('/^'.$timestamp.'(?<nick_performing>\S+) has changed mode: (?<modes>[-+][ov]+([-+][ov]+)?) (?<nicks_undergoing>\S+( \S+)*)$/', $line, $matches)) {
 			$mode_num = 0;
@@ -32,12 +32,12 @@ class parser_limechat extends parser
 			}
 		} elseif (preg_match('/^'.$timestamp.'(?<nick_performing>\S+) is now known as (?<nick_undergoing>\S+)$/', $line, $matches)) {
 			$this->set_nickchange($matches['time'], $matches['nick_performing'], $matches['nick_undergoing']);
-		} elseif (preg_match('/^'.$timestamp.'(?<nick>\S+) has left \(.*\)$/', $line, $matches)) {
+		} elseif (preg_match('/^'.$timestamp.'(?<nick>\S+) has left/', $line, $matches)) {
 			$this->set_part($matches['time'], $matches['nick']);
 		} elseif (preg_match('/^'.$timestamp.'(?<nick>\S+) has set topic: (?<line>.+)$/', $line, $matches)) {
 			$this->set_topic($matches['time'], $matches['nick'], $matches['line']);
-		} elseif (preg_match('/^'.$timestamp.'(?<line>(?<nick_performing>\S+) has kicked (?<nick_undergoing>\S+) \(.*\))$/', $line, $matches)) {
-			$this->set_kick($matches['time'], $matches['nick_performing'], $matches['nick_undergoing'], $matches['line']);
+		} elseif (preg_match('/^'.$timestamp.'(?<line>(?<nick_performing>\S+) has kicked (?<nick_undergoing>\S+) )(?<reason>\(.*\))$/', $line, $matches)) {
+			$this->set_kick($matches['time'], $matches['nick_performing'], $matches['nick_undergoing'], $matches['line'].($matches['reason'] === '()' ? '('.$matches['nick_undergoing'].')' : $matches['reason']));
 		} else {
 			out::put('debug', 'skipping line '.$this->linenum.': \''.$line.'\'');
 		}
